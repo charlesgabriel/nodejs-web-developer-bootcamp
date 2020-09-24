0c65865bd07afe0f66616134ab953eae2d26676c
charlesgabriel <charlesgabriel@hotmail.com.br>
2020-09-24 13:46:29 -0300

    change the algorithm

--- 01-introduction-to-javaScript-programming/simpleMultiplication.js
+++ 01-introduction-to-javaScript-programming/simpleMultiplication.js
@@ -1,8 +1,13 @@
 let entries = ['3 9', '-30 10', '0 9'];
+
 entries.forEach((entry) => {
+  mult(entry);
+});
+
+function mult(entry) {
   let line = entry.split(" ");
   let a = parseInt(line[0]);
   let b = parseInt(line[1]);
   let total = a * b;
-  console.log("Entry = "+ line +" / X = " + total);
-});
+  console.log("Entry = "+ line +" / X = " + total); 
+}
--- 01-introduction-to-javaScript-programming/visitAtTheFair.js
+++ 01-introduction-to-javaScript-programming/visitAtTheFair.js
@@ -1,8 +1,13 @@
 let entries = ['11 7', '-11 6', '11 -2'];
+
 entries.forEach((entry) => {
+  sum(entry);
+});
+
+function sum(entry) {
   let line = entry.split(" ");
   let a = parseInt(line[0]);
   let b = parseInt(line[1]);
   let total = a + b;
-  console.log("Entry = "+ line +" / X = " + total);
-});
+  console.log("Entry = "+ line +" / X = " + total); 
+}
