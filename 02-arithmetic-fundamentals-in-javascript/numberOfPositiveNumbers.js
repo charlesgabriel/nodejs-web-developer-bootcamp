800611c9d25bb16ee1b4eb2f84795207976c0ef3
charlesgabriel <charlesgabriel@hotmail.com.br>
2020-09-24 23:49:44 -0300

    lines were organized

--- 02-arithmetic-fundamentals-in-javascript/numberOfPositiveNumbers.js
+++ 02-arithmetic-fundamentals-in-javascript/numberOfPositiveNumbers.js
@@ -1,20 +1,3 @@
-// const values = [7, -5, 6, -3.4, 4.6, 12];
-
-// employees.forEach((employee) => {
-//   calcEmployeeSalary(employee);
-// });
-
-// values.forEach((value) => {
-//   value = gets();
-// });
-
-// let positiveNumbers = values.reduce((total, value) => {
-//   if (value > 0) return total = total + 1;
-  
-//   return total;
-// }, 0);
-// console.log(positiveNumbers + "valores positivos");
-
 const numbers = [
   ['7', '-5', '6', '-3.4', '4.6', '12'],
   ['70', '67', '77', '99', '23', '1']
@@ -27,7 +10,6 @@ numbers.forEach((number) => {
 function consult(numbers) {
   let positiveNumbers = numbers.reduce((total, value) => {
     if (value > 0) return total = total + 1;
-    
     return total;
   }, 0);
   console.log(positiveNumbers + " valores positivos");
